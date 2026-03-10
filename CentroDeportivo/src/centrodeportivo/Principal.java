9e849d07e2efd1d89d988acd5f1e689818ec8f8c
vespertino <vespertino@ALUMNO14AULA26>
2026-03-10 17:51:44 +0100

    version Saul, Iago

--- CentroDeportivo/src/centrodeportivo/Principal.java
+++ CentroDeportivo/src/centrodeportivo/Principal.java
@@ -3,7 +3,7 @@ package centrodeportivo;
 public class Principal {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("soy iago");
 		System.out.println("soy Saul y tengo 18 años");
 	}
 
