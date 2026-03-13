855d5b586aa85721b3621f0afd90104c144f7574
iagov <iagov@LAPTOP-O2OA6ID8>
2026-03-13 14:18:20 +0100

    iago-actividad2relaciones

--- CentroDeportivo/src/centrodeportivo/modelo/Instalacion.java
+++ CentroDeportivo/src/centrodeportivo/modelo/Instalacion.java
@@ -1,5 +1,7 @@
 package centrodeportivo.modelo;
 
+import java.util.List;
+
 public class Instalacion {
 	
 	private int idInstalacion;
@@ -7,15 +9,17 @@ public class Instalacion {
 	private int capacidadMaxima;
 	private TipoInstalacion tipo;
 	private String horarioDisponibilidad;
+	private List < Reserva > reservas;
 	
 	public Instalacion (int idInstalacion, String nombre, int capacidadMaxima,TipoInstalacion tipo,String horarioDisponibilidad) {
 		
 		this.idInstalacion = idInstalacion ;
 		this.nombre = nombre ;
-		this.capacidadMaxima = capacidadMaxima;
-		this.tipo = tipo;
-		this.horarioDisponibilidad = horarioDisponibilidad;
-			
+		this.capacidadMaxima = capacidadMaxima ;
+		this.tipo = tipo ;
+		this.horarioDisponibilidad = horarioDisponibilidad ;
+		
+		
 	}
 
 	public int getIdInstalacion() {
@@ -37,6 +41,22 @@ public class Instalacion {
 	public String getHorarioDisponibilidad() {
 		return horarioDisponibilidad;
 	}
+	
+	public boolean verificarDisponibilidad(String fecha, String hora) {
+		
+		if (this.horarioDisponibilidad == null || !this.horarioDisponibilidad.contains(hora)) {
+			return false;
+		}
+
+		for (Reserva r : reservas) {
+			if (r.getFecha().equals(fecha) && r.getHoraInicio().equals(hora)) {
+				
+				if (r.getEstado() == Estado_Reserva.ACTIVA) {
+					return false;
+				}
+			}
+		}
+
+		return true; 
+	}
 }
-
-
