b81040dd99172f4bef9446ec7d1378a232377d16
vespertino <vespertino@ALUMNO14AULA26>
2026-03-17 15:29:35 +0100

    Cambios en las clases Reservas y añadimos un main de prueba para usar los metodos

--- CentroDeportivo/src/centrodeportivo/modelo/Reserva_Actividad_Dirigida.java
+++ CentroDeportivo/src/centrodeportivo/modelo/Reserva_Actividad_Dirigida.java
@@ -4,18 +4,26 @@ public class Reserva_Actividad_Dirigida extends Reserva {
 
 	private String nombreActividad;
 	private String monitor;
+	public static String [] monitores = {"Juan","Ana","Carlos",
+			"Lucas", "Martin", "Alejandra", "Maria"};
 	
 	public Reserva_Actividad_Dirigida(int idReserva, String fecha, String horaInicio, String duracion,
-			Estado_Reserva estado, String monitor, String nombreActividad) {
+		Estado_Reserva estado, String monitor, String nombreActividad) {
 		super(idReserva, fecha, horaInicio, duracion, estado);
-		this.monitor=monitor;
+		int num = (int)(Math.random() * monitores.length);
+		this.monitor = monitores[num];
 		this.nombreActividad=nombreActividad;
+	
 	}
 	
 	private String reglasUso;
 
 	public void setReglasUso(String reglasUso) {
-		this.reglasUso = reglasUso;
+		this.reglasUso = "Para entrar a la actividad del monitor "+monitor+", esperamos que siga las reglas especificadas por el/ella."+
+				"1. La impuntualidad es intolerable cualquier llegado fuera de tiempo perdera su oportunindad y no se le re embolsará la reserva"+
+				"2. Durante la actividad todo telefono o dispositivo electronico se pondra en una mochila propia y solo se cogeran llamadas importantes"+
+				"3. La autoridad de esta clase es del instructor/monitor y se hara lo que se pida, si no se quiere hacer o se opina excesivo puede salir y hablar con el centro"+
+				"Alguna regla particular del monitos sera notificada durante la sesion.";
 	}
 
 	public String consultarReglasUso() {
@@ -31,11 +39,11 @@ public class Reserva_Actividad_Dirigida extends Reserva {
 	}
 
 	public String getMonitor() {
-		return monitor;
+		return String.valueOf(monitor);
 	}
-
-	public void setMonitor(String monitor) {
-		this.monitor = monitor;
+	
+	public String getReglasUso() {
+		return reglasUso;
 	}
 	
 }
