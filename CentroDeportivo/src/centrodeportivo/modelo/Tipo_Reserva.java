a2be17c0226b9246e58933d10e443ecea5b46645
Saul-glez-alf <saul.gonzalez16@educa.madrid.org>
2026-04-06 16:03:06 +0200

    Update Tipo_Reserva.java

--- CentroDeportivo/src/centrodeportivo/modelo/Tipo_Reserva.java
+++ CentroDeportivo/src/centrodeportivo/modelo/Tipo_Reserva.java
@@ -1,5 +1,7 @@
 package centrodeportivo.modelo;
 
+// Enumerado que define las categorías de reservas disponibles en el centro 
+
 public enum Tipo_Reserva {
 
 	INDIVIDUAL, GRUPAL, ACTIVIDAD_DIRIGIDA;
